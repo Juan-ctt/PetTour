8286c99ca3f0384990a2bde97b307dfc5f4f0e89
MichelSou <michelprepress@gmail.com>
2023-08-15 21:35:37 -0300

    implementação do mapa INIT

--- app/src/main/java/com/example/pettour/Tela_solicitacao.java
+++ app/src/main/java/com/example/pettour/Tela_solicitacao.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Tela_solicitacao extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
+public class Tela_solicitacao extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+    private MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_solicitacao);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        // Configurar o mapa conforme necessário
     }
 }
