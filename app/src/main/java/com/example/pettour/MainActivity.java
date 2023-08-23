59a35a0f6b41cac05f9ae74f218a46b56454ee56
Juan Soares <Juan.ctt.pes@gmail.com>
2023-08-22 22:41:38 -0300

    adicionado função de logout e manter-se conectado

--- app/src/main/java/com/example/pettour/MainActivity.java
+++ app/src/main/java/com/example/pettour/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.pettour;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
     private FloatingActionButton fab;
+
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -19,15 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.drawerLayout);
-        fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
+        //SET DE FRAGMENTE DENTRO DA ACTIVITY-------------------------------------------------------
 
 
         // Criar uma instância do Fragment
@@ -37,5 +38,48 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, Fragment_solicitacao)
                 .commit();
+
+
+        //MENU LATERAL------------------------------------------------------------------------------
+        drawerLayout = findViewById(R.id.drawerLayout);
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        //CONFIGURAÇÃO DOS BOTÕES DO MENU-----------------------------------------------------------
+
+        // Inicialização do FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Configuração do NavigationView
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_logout) {
+            // Realizar o logout do Firebase
+            mAuth.signOut();
+
+            // Iniciar a TelaLogin
+            Intent loginIntent = new Intent(this, TelaLogin.class);
+            startActivity(loginIntent);
+            finish(); // Encerrar a MainActivity
+
+            return true;
+        }
+
+        // Outros  cliques de itens do menu
+
+        return false;
     }
 }
--- app/src/main/java/com/example/pettour/TelaLogin.java
+++ app/src/main/java/com/example/pettour/TelaLogin.java
@@ -1,16 +1,21 @@
 package com.example.pettour;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Switch;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.pettour.databinding.ActivityTelaLoginBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class TelaLogin extends AppCompatActivity {
 
     private ActivityTelaLoginBinding binding;
     private FirebaseAuth mAuth;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,27 @@ public class TelaLogin extends AppCompatActivity {
         binding.textCadastro.setOnClickListener(view -> {
             startActivity(new Intent(this, tela_seletor_cadastro.class));
         });
+
+        Switch switchStayLoggedIn = binding.switchManterconectado;
+        switchStayLoggedIn.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            // Usar a variável preferences definida fora do onCreate()
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("stayLoggedIn", isChecked);
+            editor.apply();
+        });
+
+        // Inicializar a variável preferences aqui
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Verificar a preferência do usuário de permanecer logado
+        boolean stayLoggedIn = preferences.getBoolean("stayLoggedIn", false);
+        if (stayLoggedIn) {
+            FirebaseUser user = mAuth.getCurrentUser();
+            if (user != null) {
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+        }
     }
 
 
