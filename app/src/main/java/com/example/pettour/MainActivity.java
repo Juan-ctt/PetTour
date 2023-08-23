e54e56997447f4f2c68aebe0e1e6701cf9c84e4c
Juan Soares <Juan.ctt.pes@gmail.com>
2023-08-22 21:25:36 -0300

    adicionado botão flutuante e menu lateral

--- app/src/main/java/com/example/pettour/MainActivity.java
+++ app/src/main/java/com/example/pettour/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.pettour;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
 
         // Criar uma instância do Fragment
         Fragment_solicitacao Fragment_solicitacao = new Fragment_solicitacao();
